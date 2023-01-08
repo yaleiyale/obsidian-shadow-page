8c582d5ebf4ef09d3070bd712fad12f75848c711
yaleiyale <wodcode@outlook.com>
2023-01-08 14:27:21 +0800

    use async

--- src/file-helper.ts
+++ src/file-helper.ts
@@ -1,54 +1,70 @@
 import * as fs from 'fs'
 import * as path from 'path'
 export function copyNote (srcPath: string, tarPath: string, filter = []): void {
-  const files = fs.readdirSync(srcPath)
-  files.forEach(filename => {
-    const filedir = path.join(srcPath, filename)
-    const filterFlag = filter.some(item => item === filename)
-    if (!filterFlag) {
-      const stats = fs.statSync(filedir)
-      if (stats.isFile()) { // 复制文件
-        const destPath = path.join(tarPath, filename)
-        fs.copyFileSync(filedir, destPath)
-      } else { // 创建文件夹
-        const tarFiledir = path.join(tarPath, filename)
-        fs.mkdirSync(tarFiledir)
-        copyNote(filedir, tarFiledir, filter) // 递归
+  fs.readdir(srcPath, (err, files) => {
+    if (err != null) { console.log(err) } else {
+      files.forEach(filename => {
+        const filedir = path.join(srcPath, filename)
+        const filterFlag = filter.some(item => item === filename)
+        if (!filterFlag) {
+          fs.stat(filedir, (err, stats) => {
+            if (err != null) { console.log(err) } else {
+              if (stats.isFile()) { // 复制文件
+                const destPath = path.join(tarPath, filename)
+                fs.copyFile(filedir, destPath, err => console.log(err))
+              } else { // 创建文件夹
+                const tarFiledir = path.join(tarPath, filename)
+                fs.mkdir(tarFiledir, err => console.log(err))
+                copyNote(filedir, tarFiledir, filter) // 递归
+              }
+            }
+          })
+        }
       }
+      )
     }
-  })
+  }
+  )
 }
 function emptyDir (path: string): void {
-  const files = fs.readdirSync(path)
-  files.forEach(file => {
-    const filePath = `${path}/${file}`
-    const stats = fs.statSync(filePath)
-    if (stats.isDirectory()) {
-      emptyDir(filePath)
-    } else {
-      fs.unlinkSync(filePath)
+  fs.readdir(path, (err, files) => {
+    if (err != null) { console.log(err) } else {
+      files.forEach(file => {
+        const filePath = `${path}/${file}`
+        fs.stat(filePath, (err, stats) => {
+          if (err != null) { console.log(err) } else {
+            if (stats.isDirectory()) {
+              emptyDir(filePath)
+            } else {
+              fs.unlink(filePath, (err) => console.log(err))
+            }
+          }
+        })
+      })
     }
   })
 }
-
 /**
  * 删除指定路径下的所有空文件夹
  * @param {*} path
  */
 function rmEmptyDir (path: string, level = 0): void {
-  const files = fs.readdirSync(path)
-  if (files.length > 0) {
-    let tempFile = 0
-    files.forEach(file => {
-      tempFile++
-      rmEmptyDir(`${path}/${file}`, 1)
-    })
-    if (tempFile === files.length && level !== 0) {
-      fs.rmdirSync(path)
+  fs.readdir(path, (err, files) => {
+    if (err != null) { console.log(err) } else {
+      if (files.length > 0) {
+        let tempFile = 0
+        files.forEach(file => {
+          tempFile++
+          rmEmptyDir(`${path}/${file}`, 1)
+        })
+        if (tempFile === files.length && level !== 0) {
+          fs.rmdir(path, (err) => console.log(err))
+        }
+      } else {
+        level !== 0 && fs.rmdir(path, (err) => console.log(err))
+      }
     }
-  } else {
-    level !== 0 && fs.rmdirSync(path)
-  }
+  })
 }
 
 /**
